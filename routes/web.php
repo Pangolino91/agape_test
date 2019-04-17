278380ede815284763fc02a98cbfb4ccd6923723
Mayank <mjain.ror@gmail.com>
2019-04-17 18:33:10 +0530

    Url genrator added

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,8 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use Illuminate\Routing\UrlGenerator::forceSchema;
+
 if (env('APP_ENV') === 'production') {
     URL::forceSchema('https');
 }
